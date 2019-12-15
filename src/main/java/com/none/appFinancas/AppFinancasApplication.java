f89ef1246af0cd517059c6616e1d64f312587b64
Matheus Gomes <matheusejesuss2@outlook.com>
2019-12-14 21:32:19 -0300

    Estrutura inicial

---
+++ src/main/java/com/none/appFinancas/AppFinancasApplication.java
@@ -0,0 +1,13 @@
+package com.none.appFinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFinancasApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/none/appFinancas/entity/User.java
@@ -0,0 +1,37 @@
+package com.none.appFinancas.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
+
