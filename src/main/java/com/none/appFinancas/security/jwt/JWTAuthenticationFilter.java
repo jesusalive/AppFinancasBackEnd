1f92c9fab2cefb05a2a8ebf75176e6633f373152
Matheus Gomes <matheusejesuss2@outlook.com>
2019-12-22 18:23:23 -0300

    Tudo funfando, API pronta

--- src/main/java/com/none/appFinancas/security/jwt/JWTAuthenticationFilter.java
+++ src/main/java/com/none/appFinancas/security/jwt/JWTAuthenticationFilter.java
@@ -2,13 +2,19 @@ package com.none.appFinancas.security.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.none.appFinancas.dto.LoginFormDTO;
+import com.none.appFinancas.dto.UserDTO;
+import com.none.appFinancas.entity.User;
 import com.none.appFinancas.errors.AuthError;
+import com.none.appFinancas.repository.UserRepository;
 import com.none.appFinancas.security.UserSS;
+import com.none.appFinancas.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -21,6 +27,9 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Autowired
+    private UserRepository userRepository;
 
     private AuthenticationManager authenticationManager;
 
@@ -57,7 +66,14 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         String username = ((UserSS) authResult.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
         response.addHeader("Authorization", "Bearer " + token);
+
+        Long userId = ((UserSS) authResult.getPrincipal()).getId();
+        response.setContentType("application/json");
+        response.getWriter().append(json(userId));
     }
 
+    private String json(Long userId) {
+        return "{\"userId\": \"" + userId + "\"}";
+    }
 
 }
