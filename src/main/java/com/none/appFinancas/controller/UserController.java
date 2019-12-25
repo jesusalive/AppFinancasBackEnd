bdfe38c9d32b045053ab87dee4cb3df4f6ab1c64
Matheus Gomes <matheusejesuss2@outlook.com>
2019-12-25 11:17:43 -0300

    Permissão de acesso nos users/id para pegar os dados do usuario logado

--- src/main/java/com/none/appFinancas/controller/UserController.java
+++ src/main/java/com/none/appFinancas/controller/UserController.java
@@ -4,6 +4,7 @@ import com.none.appFinancas.adapter.UserAdapter;
 import com.none.appFinancas.dto.UserDTO;
 import com.none.appFinancas.dto.UserFormDTO;
 import com.none.appFinancas.entity.User;
+import com.none.appFinancas.security.VerifyLoggedUser;
 import com.none.appFinancas.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -24,8 +25,8 @@ public class UserController {
     }
 
     @GetMapping("/users/{userId}")
-    @PreAuthorize("hasAnyRole('ADMIN')")
     public UserDTO findOneUser(@PathVariable Long userId) throws IllegalAccessException {
+        VerifyLoggedUser.verifyLoggedUser(userId);
         User oldUser = userService.findOne(userId);
 
         return UserAdapter.userAdapter(oldUser);
