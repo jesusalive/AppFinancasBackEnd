2258191192d86ce644fd8cf0e542dd70ad9ce127
Matheus Gomes <matheusejesuss2@outlook.com>
2019-12-25 14:38:21 -0300

    Refresh token adicionado

--- src/main/java/com/none/appFinancas/controller/LoginController.java
+++ src/main/java/com/none/appFinancas/controller/LoginController.java
@@ -4,10 +4,16 @@ import com.none.appFinancas.dto.CodeForgotPassDTO;
 import com.none.appFinancas.dto.ForgotFormDTO;
 import com.none.appFinancas.dto.LoginFormDTO;
 import com.none.appFinancas.entity.User;
+import com.none.appFinancas.security.UserSS;
+import com.none.appFinancas.security.VerifyLoggedUser;
+import com.none.appFinancas.security.jwt.JWTUtil;
 import com.none.appFinancas.service.UserService;
 import com.none.appFinancas.service.mail.SmtpMailSender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class LoginController {
@@ -18,6 +24,9 @@ public class LoginController {
     @Autowired
     private SmtpMailSender smtpMailSender;
 
+    @Autowired
+    private JWTUtil jwtUtil;
+
     @PostMapping("/login")
     public void signIn(@RequestBody LoginFormDTO form){
 
@@ -27,5 +36,13 @@ public class LoginController {
     public CodeForgotPassDTO forgotPass(@RequestBody ForgotFormDTO username){
         User user = userService.findUserByUsername(username.getUsername());
         return smtpMailSender.sendMailPassRecovery(user.getEmail(), user.getName(), user.getId());
+    }
+
+    @PostMapping("/refresh_token")
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.loggedUser();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
     }
 }
